ffec5f9a92e5176458a2aa3577da93d642a9c332
Joseph Mckenzie <joseph.p.mckenzie84@gmail.com>
2024-02-06 02:31:34 -0500

    commit before going to bed

--- src/index.ts
+++ src/index.ts
@@ -1,265 +1,230 @@
 //index.ts
-import {
-  RekognitionClient,
-  DetectTextCommand,
-  TextDetection,
-} from '@aws-sdk/client-rekognition';
+import { RekognitionClient, DetectTextCommand, TextDetection } from '@aws-sdk/client-rekognition';
 
 import * as Interfaces from './interfaces/interfaces';
 import { uploadingToS3 } from './s3Functions';
-import {
-  createSerialNumberMappings,
-  federalReserveMapping,
-} from './mappings/additional_mapping';
+import { createSerialNumberMappings, federalReserveMapping } from './mappings/additional_mapping';
 
 async function getTextDetections(imageData: string | Buffer, fileName: string) {
-  try {
-    if (!imageData || (typeof imageData === 'string' && imageData.length === 0) || (Buffer.isBuffer(imageData) && imageData.length === 0)) {
-      throw new Error('Image data is empty or undefined.');
-    }
+	try {
+		if (!imageData || (typeof imageData === 'string' && imageData.length === 0) || (Buffer.isBuffer(imageData) && imageData.length === 0)) {
+			throw new Error('Image data is empty or undefined.');
+		}
 
-    const client = new RekognitionClient({ region: 'us-east-1' });
+		const client = new RekognitionClient({ region: 'us-east-1' });
 
-    const command = new DetectTextCommand({
-      Image: {
-        Bytes: Buffer.isBuffer(imageData) ? imageData : Buffer.from(imageData, 'base64'),
-      },
-      Filters: {
-        WordFilter: {
-          MinConfidence: 50,
-        },
-      },
-    });
+		const command = new DetectTextCommand({
+			Image: {
+				Bytes: Buffer.isBuffer(imageData) ? imageData : Buffer.from(imageData, 'base64'),
+			},
+			Filters: {
+				WordFilter: {
+					MinConfidence: 50,
+				},
+			},
+		});
 
-    const response = await client.send(command);
+		const response = await client.send(command);
 
-    if (!response || !response.TextDetections || response.TextDetections.length === 0) {
-      throw new Error('No text detections found in the response.');
-    }
+		if (!response || !response.TextDetections || response.TextDetections.length === 0) {
+			throw new Error('No text detections found in the response.');
+		}
 
-    const noteDetails = await checkRegexPatterns(response.TextDetections, fileName);
+		const noteDetails = await checkRegexPatterns(response.TextDetections, fileName);
 
-    return noteDetails;
-  } catch (error: unknown) {
-    if (error instanceof Error) {
-      throw new Error('Error processing image: ' + (error.message || error));
-    } else {
-      throw new Error('Unknown error occurred');
-    }
-  }
+		return noteDetails;
+	} catch (error: unknown) {
+		if (error instanceof Error) {
+			throw new Error('Error processing image: ' + (error.message || error));
+		} else {
+			throw new Error('Unknown error occurred');
+		}
+	}
 }
 
-function getAdditionalDetails(
-  denomination: Interfaces.DenominationDetail[],
-  serialNumber: string
-): Interfaces.MatchedDetail {
-  try {
-    if (denomination) {
-      const matchedDetail = denomination.find(detail => {
-        // Check if the pattern is a RegExp or a string and create the appropriate RegExp object
-        const regex =
-          typeof detail.pattern === 'string'
-            ? new RegExp(detail.pattern)
-            : detail.pattern;
-        return regex.test(serialNumber.charAt(0));
-      });
+function getAdditionalDetails(denomination: Interfaces.DenominationDetail[], serialNumber: string): Interfaces.MatchedDetail {
+	try {
+		if (denomination) {
+			const matchedDetail = denomination.find((detail) => {
+				// Checks if the pattern is already a RegExp or if it is a string creates a RegExp object
+				const regex = typeof detail.pattern === 'string' ? new RegExp(detail.pattern) : detail.pattern;
+				return regex.test(serialNumber.charAt(0));
+			});
 
-      if (matchedDetail) {
-        return {
-          seriesYear: matchedDetail.seriesYear,
-          treasurer: matchedDetail.treasurer,
-          secretary: matchedDetail.secretary,
-        };
-      }
-    }
-    throw new Error('Error');
-  } catch (error) {
-    throw {
-      status: 400,
-      error: `Error obtaining additionalDetails, ${error}`,
-      inputDetails: {},
-      validator: 'additionalDetails',
-    };
-  }
+			if (matchedDetail) {
+				return {
+					seriesYear: matchedDetail.seriesYear,
+					treasurer: matchedDetail.treasurer,
+					secretary: matchedDetail.secretary,
+				};
+			}
+		}
+		throw new Error('Error');
+	} catch (error) {
+		throw {
+			status: 400,
+			error: `Error obtaining additionalDetails, ${error}`,
+			inputDetails: {},
+			validator: 'additionalDetails',
+		};
+	}
 }
 async function checkRegexPatterns(textDetections: TextDetection[], fileName: string) {
-  try {
-    const matchedWordsHash: Record<string, string> = {};
+	try {
+		const matchedWordsHash: Record<string, string> = {};
 
-    // Iterate over each text detection
-    for (const text of textDetections) {
-      const detectedText = text.DetectedText?.replace(/ /g, '');
+		for (const text of textDetections) {
+			const detectedText = text.DetectedText?.replace(/ /g, '');
 
-      if (detectedText) {
-        let correctedText = detectedText;
+			if (detectedText) {
+				let correctedText = detectedText;
 
-        // Check for mistaken letters
-        const correctedWord = checkPotentialMistakenLetter(detectedText);
-        if (correctedWord) {
-          correctedText = correctedWord;
-        }
+				// Check for possible mistaken identifiers
+				const correctedWord = checkPotentialMistakenLetter(detectedText);
+				if (correctedWord) {
+					correctedText = correctedWord;
+				}
 
-        // Check against unique serial number patterns
-        for (const patternKey in serialNumberPatterns) {
-          const pattern = new RegExp(serialNumberPatterns[patternKey]);
+				// Check for unique serial number patterns
+				for (const patternKey in serialNumberPatterns) {
+					const pattern = new RegExp(serialNumberPatterns[patternKey]);
 
-          if (pattern.test(correctedText)) {
-            matchedWordsHash['SerialPatternMatch'] = patternKey;
-            // You may want to break here if you only want to match against the first pattern
-            // break;
-          }
-        }
+					if (pattern.test(correctedText)) {
+						matchedWordsHash['SerialPatternMatch'] = patternKey;
+					}
+				}
 
-        // Check against note validator patterns
-        for (const patternKey in noteValidators) {
-          const pattern = new RegExp(noteValidators[patternKey]);
+				// Check to make sure that all detected identifiers match the expected format
+				for (const patternKey in noteValidators) {
+					const pattern = new RegExp(noteValidators[patternKey]);
 
-          if (pattern.test(correctedText)) {
-            matchedWordsHash[patternKey] = correctedText;
-          }
-        }
-      }
-    }
+					if (pattern.test(correctedText)) {
+						matchedWordsHash[patternKey] = correctedText;
+					}
+				}
+			}
+		}
 
-    // Get additional details based on matched patterns
-    const denomination = matchedWordsHash.validDenomination;
-    const serialNumber = matchedWordsHash.validSerialNumberPattern;
-    const additionalDetails = getAdditionalDetails(
-      createSerialNumberMappings('./src/mappings/additionalMAppingDetails.txt')[`$${denomination}`],
-      serialNumber
-    );
+		const denomination = matchedWordsHash.validDenomination;
+		const serialNumber = matchedWordsHash.validSerialNumberPattern;
 
+		// Get the series year,secetary and treasurer from our mapping details in the event tgat we couldn't read them of the uploaded note.
+		const additionalDetails = getAdditionalDetails(
+			createSerialNumberMappings('./src/mappings/additionalMappingDetails.txt')[`$${denomination}`],
+			serialNumber
+		);
 
- 
-    // Upload image to S3 and get the URL
-    // const s3Url = await uploadingToS3({ s3Url: '', serialNumber }, fileName);
+		// Upload image to S3 and get the location of the upload from the buccket 
+		// const s3Url = await uploadingToS3({ s3Url: '', serialNumber }, fileName);
 
-    // Construct details object to return
-    const details = {
-      validDenomination: matchedWordsHash.validDenomination,
-      frontPlateId: matchedWordsHash.frontPlateId,
-      SerialPatternMatch: matchedWordsHash.SerialPatternMatch,
-      serialNumber: matchedWordsHash.validSerialNumberPattern,
-      federalReserveId: matchedWordsHash.federalReserveId,
-      federalReserveLocation:'',
-      notePositionId: matchedWordsHash.notePositionId,
-      seriesYear: additionalDetails?.seriesYear,
-      treasurer: additionalDetails?.treasurer,
-      secretary: additionalDetails?.secretary,
-      s3Url: '',
-    };
-  
-   // const federalReserveId = noteDetails.federalReserveId;
-   if (matchedWordsHash.federalReserveId && federalReserveMapping[matchedWordsHash.federalReserveId]) {
-    details.federalReserveLocation =
-    federalReserveMapping[matchedWordsHash.federalReserveId];
+		const details = {
+			validDenomination: matchedWordsHash.validDenomination,
+			frontPlateId: matchedWordsHash.frontPlateId,
+			SerialPatternMatch: matchedWordsHash.SerialPatternMatch,
+			serialNumber: matchedWordsHash.validSerialNumberPattern,
+			federalReserveId: matchedWordsHash.federalReserveId,
+			federalReserveLocation: '',
+			notePositionId: matchedWordsHash.notePositionId,
+			seriesYear: additionalDetails?.seriesYear,
+			treasurer: additionalDetails?.treasurer,
+			secretary: additionalDetails?.secretary,
+			s3Url: '',
+		};
+
+		if (matchedWordsHash.federalReserveId && federalReserveMapping[matchedWordsHash.federalReserveId]) {
+			details.federalReserveLocation = federalReserveMapping[matchedWordsHash.federalReserveId];
+		}
+
+		await uploadingToS3(details, fileName);
+
+		return details; 
+	} catch (error) {
+		throw new Error(`Error in checkRegexPatterns ${JSON.stringify(error)}`);
+	}
 }
-
-
-    await uploadingToS3(details , fileName);
-
-
-    return details; // Return the constructed details object
-  } catch (error) {
-    throw new Error(`Error in checkRegexPatterns ${JSON.stringify(error)}`);
-  }
-}
-
 
 function checkPotentialMistakenLetter(detectedText: string) {
-  if (/[A-L]\s?[i]/.test(detectedText)) {
-    return detectedText.replace(/Ei/g, 'E1');
-  }
-  return null;
+	if (/[A-L]\s?[i]/.test(detectedText)) {
+		return detectedText.replace(/Ei/g, 'E1');
+	}
+	return null;
 }
 
 const serialNumberPatterns: Interfaces.RegExValidators = {
-  // Example: Check if a serial number matches the low serial number pattern
-  isLowSerialNumberPattern: /^[A-Q]?[A-L]\s?0{4}\d{4}\s?[A-Q]$/,
-  // Examples: ' AB00000011C, BL 00001234 D , DA 00000001L'
+	// Checks to see if a serial number matches a low serial number pattern
+	isLowSerialNumberPattern: /^[A-Q]?[A-L]\s?0{4}\d{4}\s?[A-Q]$/,
+	// Examples: ' AB00000011C, BL 00001234 D , DA 00000001L'
 
-  // Example: Check if a serial number matches the high serial number pattern
-  isHighSerialNumberPattern: /^[A-Q]?[A-L]\s?9{4}\d{4}\s?[A-Q]$/,
-  // Examples: 'AB99999911C, BL 99991234 D , DA 99999991L'
+	// Checks to see if a serial number matches a high serial number pattern
+	isHighSerialNumberPattern: /^[A-Q]?[A-L]\s?9{4}\d{4}\s?[A-Q]$/,
+	// Examples: 'AB99999911C, BL 99991234 D , DA 99999991L'
 
-  // Example: Check if a serial number matches the ladder pattern
-  isLadderSerialPattern:
-    /^(?:\d{8}|01234567|12345678|23456789|87654321|98765432)$/,
-  // Examples: 'A12345678D,DL98765432D'
+	// Checks to see if the serial number matches a ladder pattern
+	isLadderSerialPattern: /^(?:\d{8}|01234567|12345678|23456789|87654321|98765432)$/,
+	// Examples: 'A12345678D,DL98765432D'
 
-  // Checks to see if a serial number contains all the same digits.
-  isASolidNumberSerialPattern:
-    /^(?:[A-Q]?[A-L](?:\s?(\d)\1{7})(?:\s?[A-Q])|(\d)\2{7})$/,
-  // Examples:',A11111111D,BA77777777C,CD22222222A'
+	// Checks to see if a serial number contains all the same digits.
+	isASolidNumberSerialPattern: /^(?:[A-Q]?[A-L](?:\s?(\d)\1{7})(?:\s?[A-Q])|(\d)\2{7})$/,
+	// Examples:',A11111111D,BA77777777C,CD22222222A'
 
-  // Radar Serial Number - A radar is where the second four digits are the reverse of the first four digits.
-  isRadarSerialPattern:
-    /^[A-Q]?[A-L](?:\s?(\d)(\d)(\d)(\d)\4\3\2\1)(?:\s?(\d)(\d)(\d)(\d)\1\2\3\4)(?:\s?[A-Q])$/,
-  // // Examples: 'AC11188111G,LA20066002I,LM12344321A'
+	// Radar Serial Number - A radar is where the second four digits are the reverse of the first four digits.
+	isRadarSerialPattern: /^[A-Q]?[A-L](?:\s?(\d)(\d)(\d)(\d)\4\3\2\1)(?:\s?(\d)(\d)(\d)(\d)\1\2\3\4)(?:\s?[A-Q])$/,
+	// // Examples: 'AC11188111G,LA20066002I,LM12344321A'
 
-  // Radar Repeater - A radar repeater is both a radar and a repeater.
-  isRadarRepeaterSerialPattern:
-    /^[A-Q]?[A-L](?:\s?(?=\d*$)\d*(\d{4})\d*\1\d*)(?:[[:blank:]][A-Q])$/,
-  // Examples: '01100110,02200220,98899889'
+	// Radar Repeater - A radar repeater is both a radar and a repeater.
+	isRadarRepeaterSerialPattern: /^[A-Q]?[A-L](?:\s?(?=\d*$)\d*(\d{4})\d*\1\d*)(?:[[:blank:]][A-Q])$/,
+	// Examples: '01100110,02200220,98899889'
 
-  // Super Radar - In a super radar, the interior digits are all the same.
-  isSuperRadarSerialPattern:
-    /^[A-Q]?[A-L](?:\s?(\d)\1*([^\1]\1*))(?:[[:blank:]][A-Q])$/,
-  // Examples: '71111117,98888889,21111112'
+	// Super Radar - In a super radar, the interior digits are all the same.
+	isSuperRadarSerialPattern: /^[A-Q]?[A-L](?:\s?(\d)\1*([^\1]\1*))(?:[[:blank:]][A-Q])$/,
+	// Examples: '71111117,98888889,21111112'
 
-  // // Super Repeater - In a super repeater, the first two digits are repeated four times.
-  isSuperRepeaterSerialPattern:
-    /^[A-Q]?[A-L](?:\s?(\d{2})\1{3})(?:[[:blank:]][A-Q])$/,
-  // Examples: '01010101, 02020202, 03030303'
+	// // Super Repeater - In a super repeater, the first two digits are repeated four times.
+	isSuperRepeaterSerialPattern: /^[A-Q]?[A-L](?:\s?(\d{2})\1{3})(?:[[:blank:]][A-Q])$/,
+	// Examples: '01010101, 02020202, 03030303'
 
-  // Double Quad - In a double quad serial number, the first four digits and the second four digits are the same.
-  isDoubleQuadSerialPattern:
-    /^[A-Q]?[A-L](?:\s?(\d{4})\1)(?:[[:blank:]][A-Q])$/,
-  // Examples: '12341234,08800880,24202420'
+	// Double Quad - In a double quad serial number, the first four digits and the second four digits are the same.
+	isDoubleQuadSerialPattern: /^[A-Q]?[A-L](?:\s?(\d{4})\1)(?:[[:blank:]][A-Q])$/,
+	// Examples: '12341234,08800880,24202420'
 
-  // Seven-Of-A-Kind - In a seven-of-a-kind serial number, seven digits are all the same.
-  isSevenOfAKindSerialPattern:
-    /^[A-Q]?[A-L](?:\s?(\d)(?:.*?\1){6})(?:[[:blank:]][A-Q])$/,
-  // // Example: '11112111'
+	// Seven-Of-A-Kind - In a seven-of-a-kind serial number, seven digits are all the same.
+	isSevenOfAKindSerialPattern: /^[A-Q]?[A-L](?:\s?(\d)(?:.*?\1){6})(?:[[:blank:]][A-Q])$/,
+	// // Example: '11112111'
 
-  // Seven-In-A-Row - In a seven-in-a-row serial number, seven consecutive digits are all the same.
-  isSevenInArowSerialPattern:
-    /^[A-Q]?[A-L](?:\s?(\d)\1{6})(?:[[:blank:]][A-Q])$/,
-  // // Example: '11111110'
+	// Seven-In-A-Row - In a seven-in-a-row serial number, seven consecutive digits are all the same.
+	isSevenInArowSerialPattern: /^[A-Q]?[A-L](?:\s?(\d)\1{6})(?:[[:blank:]][A-Q])$/,
+	// // Example: '11111110'
 
-  // Binary - In a binary serial number, only two numbers appear.
-  isBinarySerialPattern:
-    /^[A-Q]?[A-L](?:\s?(?!(\d)\1*$)([0-9])\d*([0-9])\d*)(?:[[:blank:]][A-Q])$/,
-  // // Examples: '10101010,12122112,34343434'
+	// Binary - In a binary serial number, only two numbers appear.
+	isBinarySerialPattern: /^[A-Q]?[A-L](?:\s?(?!(\d)\1*$)([0-9])\d*([0-9])\d*)(?:[[:blank:]][A-Q])$/,
+	// // Examples: '10101010,12122112,34343434'
 
-  // True Binary - In a true binary serial number, those two numbers are zero and one.
-  isTrueBinarySerialPattern: /^[A-Q]?[A-L](?:\s?(01|10)+)(?:[[:blank:]][A-Q])$/,
-  // Example: '01010101'
+	// True Binary - In a true binary serial number, those two numbers are zero and one.
+	isTrueBinarySerialPattern: /^[A-Q]?[A-L](?:\s?(01|10)+)(?:[[:blank:]][A-Q])$/,
+	// Example: '01010101'
 
-  // Example: Check if a serial number contains the number 420
-  fourTwentySerialPattern:
-    /^(?:[A-Q]\s?[A-L]?(?:\s?|\s)(\d*420\d*)(?:\s?[A-Q]))$/,
+	// Example: Check if a serial number contains the number 420
+	fourTwentySerialPattern: /^(?:[A-Q]\s?[A-L]?(?:\s?|\s)(\d*420\d*)(?:\s?[A-Q]))$/,
 };
 
 const noteValidators: Interfaces.RegExValidators = {
-  // Valid Serial Number Pattern - Check if a serial number matches the serial number pattern(s)
-  validSerialNumberPattern: /^[A-Q]?[A-L]\s?\d{8}\s?[A-K]$/,
-  // Examples: 'AB12345678C,AB 12345678 C, PA42003287 D, LD 56709823E'
+	// Valid Serial Number Pattern - Check if a serial number matches the serial number pattern(s)
+	validSerialNumberPattern: /^[A-Q]?[A-L]\s?\d{8}\s?[A-K]$/,
+	// Examples: 'AB12345678C,AB 12345678 C, PA42003287 D, LD 56709823E'
 
-  // Makes Sure that the federal reserve ID is in the correct format(s).
-  federalReserveId:
-    /^A1$|^B2$|^C3$|^D4$|^E5$|^F6$|^G7$|^H8$|^I9$|^J10$|^K11$|^L12$/,
+	// Makes Sure that the federal reserve ID is in the correct format(s).
+	federalReserveId: /^A1$|^B2$|^C3$|^D4$|^E5$|^F6$|^G7$|^H8$|^I9$|^J10$|^K11$|^L12$/,
 
-  // Checks that note position has a Letter ranging from A-J followed by any number from ranging 1-5
-  notePositionId: /^([A-J])\S?[1-5]$/,
-  // Examples- 'F3,B2,H 2'
+	// Checks that note position has a Letter ranging from A-J followed by any number from ranging 1-5
+	notePositionId: /^([A-J])\S?[1-5]$/,
+	// Examples- 'F3,B2,H 2'
 
-  frontPlateId: /^(FW)\s?([A-J])\s?(\d{1,4})$/,
-  // Examples - 'FW A 1, B2, E 5, FW L 122, FW B999'
+	frontPlateId: /^(FW)\s?([A-J])\s?(\d{1,4})$/,
+	// Examples - 'FW A 1, B2, E 5, FW L 122, FW B999'
 
-  validDenomination: /^(?:1|2|5|10|20|50|100)$/,
-  // Examples - '1,2,5,10,20,50,100's
+	validDenomination: /^(?:1|2|5|10|20|50|100)$/,
+	// Examples - '1,2,5,10,20,50,100's
 };
 
-export {getTextDetections};
+export { getTextDetections };
 // getTextDetections('./MK.jpeg');
